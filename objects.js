8e9a08851ed9d9dc63561a88cd10733654094f01
samsam145 <67905530+samsam145@users.noreply.github.com>
2020-08-18 19:28:05 +0100

    Initial commit

---
+++ objects.js
@@ -0,0 +1,7 @@
+var movie = { title: "Moana", //string 
+releaseYear: 2016, //number
+cast:{ "Aali'i Cravalgo", "Dwayne Johnson", "Alan Todyk"}, review:{ author: 'Samsam Mohammed', rating:5,opinion:" Best movie ever!" }
+
+
+}
+ 
---
+++ theDOM.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Your code here...
+
+var par2= document.createElement("p");
+par2.innerText= "My name is Samsam and I just graduated :)"
+par2.style.fontSize= "18px"; 
+par2.style.fontFamily= "sans-serif"; 
+par2.style.width= "100px";
+par2.style.border=" 1px solid orange";
+par2.style.padding= "30px"; 
+
+document.body.appendChild(par2); 
+
+par2.addEventListener("mouse enter", )
+function(event) event.target.style.color="light blue"; }); 
+});
+
+par2.addEventListener("mouse leave"), function(event) event.target.style.color= "black"
+}); 
+
+
